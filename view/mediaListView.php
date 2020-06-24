bed10063e7f67a9aebb1b86e4c2293645b390bd0
SarahHayat <saraahyt@gmail.com>
2020-06-24 18:48:46 +0200

    change root for search

--- view/mediaListView.php
+++ view/mediaListView.php
@@ -16,10 +16,10 @@
         <form method="get">
             <div class="form-group has-btn">
 
-                    <input type="search" id="search" name="title" class="form-control"
-                           placeholder="Rechercher un film ou une série" value="">
+                <input type="search" id="search" name="title" class="form-control"
+                       placeholder="Rechercher un film ou une série" value="">
 
-                    <input type="submit" value="Valider " class="btn btn-block bg-red"/>
+                <input type="submit" value="Valider " class="btn btn-block bg-red"/>
 
             </div>
         </form>
@@ -30,32 +30,39 @@ $search = isset($_GET['title']) ? htmlspecialchars($_GET['title']) : null;
 $searchs = Media::filterMedias($search);
 
 
-if (isset($search) && strlen ($search)>0) {
+if (isset($search) && strlen($search) > 0) {
 
     ?>
     <div class="media-list">
         <div class="list-group">
             <h1>Recherche ...</h1>
             <div class="list">
-            <?php
-            foreach ($searchs as $media) {
-                ?>
-                <a class="item" href="view/detailFilm.php?media=<?= $media['id']; ?>">
-<!--                    <div id="player"></div>-->
-                        <div>
-                            <iframe allowfullscreen="" frameborder="0" allow="picture-in-picture"
-                                    src="<?= $media['trailer_url']; ?>"></iframe>
-                        </div>
-                    <form action="">
-                        <input type="hidden" value="" name="time" id="time">
-                    </form>
-
-                    <div class="title"><?= $media['title']." (" .$media['release_date'].")"?></div>
-                    <div class="title"><?= $media['time_media']?></div>
-                </a>
                 <?php
-            }
-            ?>
+                foreach ($searchs as $media) {
+                if ($media['type'] == "film") { ?>
+                <a class="item" href="view/detailFilm.php?media=<?= $media['id']; ?>">
+                    <?php
+                    }else{ ?>
+                    <a class="item" href="view/detailSaison.php?media=<?= $media['id']; ?>">
+                        <?php
+                        }
+                        ?>
+<!--                        <a class="item" href="view/detailFilm.php?media=--><?//= $media['id']; ?><!--">-->
+                            <!--                    <div id="player"></div>-->
+                            <div>
+                                <iframe allowfullscreen="" frameborder="0" allow="picture-in-picture"
+                                        src="<?= $media['trailer_url']; ?>"></iframe>
+                            </div>
+                            <form action="">
+                                <input type="hidden" value="" name="time" id="time">
+                            </form>
+
+                            <div class="title"><?= $media['title'] . " (" . $media['release_date'] . ")" ?></div>
+                            <div class="title"><?= $media['time_media'] ?></div>
+                        </a>
+                        <?php
+                        }
+                        ?>
             </div>
         </div>
     </div>
@@ -73,15 +80,15 @@ if (isset($search) && strlen ($search)>0) {
             foreach ($medias as $media) {
                 if ($media['type'] == "film") { ?>
                     <a class="item" href="view/detailFilm.php?media=<?= $media['id']; ?>">
-<!--                        <div id="player"></div>-->
-                            <div>
-                                <iframe allowfullscreen="" frameborder="0" allow="picture-in-picture"
-                                        src="<?= $media['trailer_url']; ?>"></iframe>
-                            </div>
+                        <!--                        <div id="player"></div>-->
+                        <div>
+                            <iframe allowfullscreen="" frameborder="0" allow="picture-in-picture"
+                                    src="<?= $media['trailer_url']; ?>"></iframe>
+                        </div>
 
-                        <div class="title"><?= $media['title']?></div>
+                        <div class="title"><?= $media['title'] ?></div>
 
-                        <div class="title"><?= $media['time_media']?></div>
+                        <div class="title"><?= $media['time_media'] ?></div>
 
                     </a>
                 <?php }
@@ -98,14 +105,14 @@ if (isset($search) && strlen ($search)>0) {
             foreach ($medias as $media) {
                 if ($media['type'] == "serie") { ?>
                     <a class="item" href="view/detailSaison.php?media=<?= $media['id']; ?>">
-<!--                        <div id="player"></div>-->
-                            <div>
-                                <iframe allowfullscreen="" frameborder="0"
-                                        src="<?= $media['trailer_url']; ?>"></iframe>
-                            </div>
+                        <!--                        <div id="player"></div>-->
+                        <div>
+                            <iframe allowfullscreen="" frameborder="0"
+                                    src="<?= $media['trailer_url']; ?>"></iframe>
+                        </div>
 
                         <div class="title"><?= $media['title'] ?></div>
-                        <div class="title"><?= $media['time_media']?></div>
+                        <div class="title"><?= $media['time_media'] ?></div>
 
                     </a>
                 <?php }
@@ -116,7 +123,6 @@ if (isset($search) && strlen ($search)>0) {
 </div>
 <script scr="https://www.youtube.com/iframe_api"></script>
 <script src="public/js/media.js"></script>
-
 
 
 <?php $content = ob_get_clean(); ?>
